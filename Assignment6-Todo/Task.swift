e38b868905745906847c66be131f283f54e4391f
Tomona Sako <sakotomona@gmail.com>
2020-05-14 12:07:39 -0700

    first commit

---
+++ Assignment6-Todo/Task.swift
@@ -0,0 +1,29 @@
+//
+//  Task.swift
+//  Assignment6-Todo
+//
+//  Created by Tomona Sako on 2020/05/14.
+//  Copyright © 2020 Tomona Sako. All rights reserved.
+//
+
+import Foundation
+
+enum Priority: Int  {
+    case high = 0
+    case middle = 1
+    case low = 2
+}
+
+struct Task {
+    let name: String
+    var priority: Priority
+    
+    init(name: String) {
+        self.name = name
+        priority = .middle
+    }
+    
+    mutating func changePriority(priority: Priority) {
+        self.priority = priority
+    }
+}
